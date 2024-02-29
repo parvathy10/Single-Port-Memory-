7a4745d771f72e90ec28ae2d95f54a7435845fb0
Parvathy Harikumar <parvathyharikumar01@gmail.com>
2024-02-29 16:45:30 +0530

    modified commit

--- design_1_wrapper.v
+++ design_1_wrapper.v
@@ -1,12 +1,12 @@
 //Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
-//--------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------
 //Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
 //Date        : Sun Feb  4 12:09:50 2024
 //Host        : kiwi running 64-bit major release  (build 9200)
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
 //Purpose     : IP block netlist
-//--------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------
 `timescale 1 ps / 1 ps
 
 module design_1_wrapper
